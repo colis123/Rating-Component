60cb9294a435c65245bced95fc4f0186589ff97c
Pedro Colis <colis@Pedros-MacBook-Pro.local>
2022-05-01 21:03:43 -0700

    finished styling/markup

---
+++ main.js
@@ -0,0 +1,17 @@
+// let numbers = [1, 2, 3, 4, 5];
+
+// let rating = document.getElementsByClassName('rating')
+// console.log(rating);
+
+// function ratingNumbers () {
+//     numbers.forEach(function(number) {
+//         `<div class="number">${number}</div>`
+//     });
+
+// }
+
+// console.log(rating);
+
+// //<div class="number"></div>
+
+// ratingNumbers()
